6d9f79d9defadd25807a3ee4d09293bf1d19f07d
Mike Davis <mikedavisrocha@gmail.com>
2023-03-06 09:54:35 -0300

    Finished character punch detection and animation

---
+++ Assets/Scripts/CharacterPunch.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CharacterPunch : MonoBehaviour
+{
+    [SerializeField] private float punchForce = 10f;
+    [SerializeField] private Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Apply force to the enemy
+            Vector3 punchDirection = (other.transform.position - transform.position).normalized;
+            other.GetComponent<Rigidbody>().AddForce(punchDirection * punchForce, ForceMode.Impulse);
+
+            // Play the punch animation
+            animator.SetTrigger("Punch");
+
+            // Deactivate Enemy Animator
+            other.GetComponent<Animator>().enabled = false;
+        }
+    }
+}
